f72b904a7c0e49cfae3c03b34f70fe4e587354cf
Niels Bjørn Christensen <201705683@post.au.dk>
2022-11-03 21:05:06 +0100

    ECDSA.py is working and can now verify and sign messages.

--- src/ECDSA.py
+++ src/ECDSA.py
@@ -4,48 +4,84 @@ from Crypto.Hash import SHA256
 import ecdsa.curves
 
 
-def hashSHA256(message):
+class EllipticCurve():
+    def __init__(self, generator):
+        self.generator = generator
+        self.n = self.generator.order()
+        self.p = self.generator.curve().p()
+        #        self.h = self.generator.curve().h()
+        self.a = self.generator.curve().a()
+        self.b = self.generator.curve().b()
+        self.x = self.generator.x()
+        self.y = self.generator.y()
+
+
+def hash_SHA256(message):
     h = SHA256.new()
     h.update(message)
-    return h.hexdigest()
+
+    message_hash = h.hexdigest()
+    message_hash_int = int(message_hash, 16)
+    message_hash_binary = format(message_hash_int, 'b')
+    n_bit_length = EC.n.bit_length()
+    hash = message_hash_binary[:n_bit_length]
+    hash = int(hash)
+
+    return hash
 
 
-def sign(message, sk, p, generator, n):
-    k = getRandomK(p)
-    r_x = generator.x() * k
-    message_hash = hashSHA256(message)
-    s = (int(message_hash, 16) + sk * r_x) * pow(k, -1, n)
-    s = s % n
-    return s, r_x
+def sign_message(message, sk, EC: EllipticCurve):
+    # Hash the message
+    message_hash = hash_SHA256(message)
+
+    # Compute r
+    # If r is zero choose another cryptographic random k.
+    r = 0
+    while r == 0:
+        # Pick random cryptographic random
+        k = getRandomK(EC)
+
+        # Calculating point on curve.
+        curve_point = k * EC.generator
+
+        r = curve_point.x() % EC.n
+
+    s = pow(k, -1, EC.n) * (message_hash + r * sk) % EC.n
+
+    return s, r
 
 
-def getRandomK(order):
-    randomint = random.randint(1, order)
-    return randomint
+def getRandomK(EC: EllipticCurve):
+    max_int = EC.n - 1
+    random_int = random.randint(1, max_int)
+    return random_int
 
 
-def verify(message, s, r_x, n, generator, pk, p):
-    hash_message = hashSHA256(message)
-    s_inverse = pow(int(s), -1, n)
-    assert int(hash_message, 16) < p
-    y = int(hash_message, 16) * generator.x() + r_x * pk
-    random_point = y * s_inverse
+def verify_signature(message, s, r_x, pk):
+    # Hash the message
+    message_hash = hash_SHA256(message)
 
-    return r_x == random_point
+    # Find S inverse
+    s_inverse = pow(s, -1, EC.n)
+    curve_point = (message_hash * s_inverse) % EC.n * EC.generator + (r_x * s_inverse) % EC.n * pk
+
+    return r_x == curve_point.x()
 
 
 if __name__ == '__main__':
-    n = ecdsa.curves.SECP256k1.order
-    p = ecdsa.curves.SECP256k1.curve.p()
-    assert n < p
-    generator = ecdsa.curves.SECP256k1.generator
-    sk = getRandomK(p)
+    EC = EllipticCurve(generator=ecdsa.curves.SECP256k1.generator)
 
-    pk = sk * generator.x()
+    message = b'Det var sku et godt link christian (kakadue)'
 
-    message = b'hello'
+    # Generate a random int as secret key
+    sk = getRandomK(EC=EC)
 
-    s, r_x = sign(message, sk, p, generator, n)
+    # Create a public key
+    pk = sk * EC.generator
 
-    result = verify(message, s, r_x, n, generator, pk, p)
-    print(result)
+    # Sign message
+    s, r = sign_message(message=message, sk=sk, EC=EC)
+
+    # Verify signature on message
+    result = verify_signature(message=message, s=s, pk=pk, r_x=r)
+    print("Message:", message.decode('ascii'), "verification on signature is:", result)
