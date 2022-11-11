be572a6471420bd30a29eefbb509164a573e3d2c
nielsbjorn-da <54630810+nielsbjorn-da@users.noreply.github.com>
2022-11-11 12:10:37 +0100

    user_independent_preprocessing and user_dependent_preprocessing is done. Code can't run, as it's not finished yet.

--- src/BeDOZa_arithmetic/threshold_ecdsa.py
+++ src/BeDOZa_arithmetic/threshold_ecdsa.py
@@ -1,55 +1,78 @@
 import random
 
 import ecdsa
+from bob import Bob
+from alice import Alice
+from dealer import Dealer
+from util import mult_two_wires
 
 from src.own_ecdsa import EllipticCurve
 
 
-def convert(secret_share):
-    EC = EllipticCurve(generator=ecdsa.curves.SECP256k1.generator)
-    secret_curve_point = EC.generator * secret_share
-    return secret_curve_point
+class threshold_ecdsa():
+    def __init__(self):
+        self.EC = EllipticCurve(generator=ecdsa.curves.SECP256k1.generator)
+        sk_a, sk_b, pk = self.key_gen()
+        self.alice = Alice()
+        self.alice.sk_a = sk_a
+        self.bob = Bob()
+        self.bob.sk_b = sk_b
+        self.dealer = Dealer
 
+    def convert(self, secret_share):
+        secret_curve_point = self.EC.generator * secret_share
+        return secret_curve_point
 
-def open_curve_point(secret_share_point_a, secret_share_point_b):
-    curve_point = (secret_share_point_a + secret_share_point_b)
-    return curve_point
+    def open_curve_point(self, secret_share_point_a, secret_share_point_b):
+        curve_point = (secret_share_point_a + secret_share_point_b)
+        return curve_point
 
-def key_gen():
-    EC = EllipticCurve(generator=ecdsa.curves.SECP256k1.generator)
+    def key_gen(self):
+        # Generate a secret key
+        sk = random.randint(0, self.EC.p)
 
-    # Generate a secret key
-    sk = random.randint(0, EC.p)
+        # Generate secret shares for both alice and bob
+        secret_share_alice = random.randint(0, self.EC.p)
+        secret_share_bob = (sk - secret_share_alice) % self.EC.p
+        # Generate public key
+        pk = self.open_curve_point(self.convert(secret_share_alice), self.convert(secret_share_bob))
 
-    # Generate secret shares for both alice and bob
-    secret_share_alice = random.randint(0, EC.p)
-    secret_share_bob = (sk - secret_share_alice) % EC.p
-    # Generate public key
-    pk = open_curve_point(convert(secret_share_alice), convert(secret_share_bob))
+        return secret_share_alice, secret_share_bob, pk
 
-    return secret_share_alice, secret_share_bob, pk
+    def user_independent_preprocessing(self):
+        #Step 1
+        dealer = Dealer(order=self.EC.p)
+        alice = self.alice
+        bob = self.bob
+        alice.randomness_from_dealer, bob.randomness_from_dealer = dealer.create_u_v_w()
 
+        # Step 2
+        c = alice.open(alice.randomness_from_dealer[2], bob.randomness_from_dealer[2])
 
-def user_independent_preprocessing():
-    EC = EllipticCurve(generator=ecdsa.curves.SECP256k1.generator)
-    a = random.randint(0, EC.p)
-    secret_share_a_a = random.randint(0, EC.p)
-    secret_share_a_b = (a - secret_share_a_a) % EC.p
+        #Step 3
+        alice.k_inverse = alice.randomness_from_dealer[0]
+        bob.k_inverse = bob.randomness_from_dealer[0]
 
-    b = random.randint(0, EC.p)
-    secret_share_b_a = random.randint(0, EC.p)
-    secret_share_b_b = (a - secret_share_b_a) % EC.p
+        #Step 4
+        c_inverse = pow(c, -1, self.EC.p)  # TODO: n or p
+        b_a = alice.randomness_from_dealer[1]
+        b_b = bob.randomness_from_dealer[1]
+        alice.curve_k_a = alice.convert(b_a) * c_inverse
+        bob.curve_k_b = bob.convert(b_b) * c_inverse
 
-    c = random.randint(0, EC.p)
-    secret_share_c_a = random.randint(0, EC.p)
-    secret_share_c_b = (a - secret_share_c_a) % EC.p
+        #Step 5
+        print("hej, user independent finish")
 
-    pass
+    def user_dependent_preprocessing(self):
+        alice = self.alice
+        bob = self.bob
+        rand_alice, rand_bob = self.dealer.create_u_v_w()
+        sk_prime_a, sk_prime_b = mult_two_wires(alice, bob, alice.k_inverse, bob.k_inverse, alice.sk_a, bob.sk_b, rand_alice, rand_bob)
+        
+        alice.sk_prime_a = sk_prime_a
+        bob.sk_prime_b = sk_prime_b
 
-
-def user_dependent_preprocessing():
-    pass
-
-
-def sign_message():
-    pass
+    def sign_message(self, message):
+        self.user_independent_preprocessing()
+        self.user_dependent_preprocessing()
+        pass
--- src/BeDOZa_arithmetic/user.py
+++ src/BeDOZa_arithmetic/user.py
@@ -1,13 +1,16 @@
-
+import threshold_ecdsa
 
 class User:
     def __init__(self, name):
         self.wallet = 1000
         self.name = name
+        self.sk = 123
+        self.pk = 321
+        self.threshold_ecdsa = threshold_ecdsa()
 
     def transfer(self):
         pass
 
-    def sign(self):
-        #RUN Threshold_ECDSA
+    def sign(self, message):
+        threshold_ecdsa.sign_message(message)
         pass
