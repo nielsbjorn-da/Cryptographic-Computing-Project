d56976c7133f51da27548991cd74e12b2bb04e07
CHSimonsen <42768488+CHSimonsen@users.noreply.github.com>
2022-11-11 11:08:48 +0100

    Create user.py

---
+++ src/BeDOZa_arithmetic/user.py
@@ -0,0 +1,13 @@
+
+
+class User:
+    def __init__(self, name):
+        self.wallet = 1000
+        self.name = name
+
+    def transfer(self):
+        pass
+
+    def sign(self):
+        #RUN Threshold_ECDSA
+        pass
