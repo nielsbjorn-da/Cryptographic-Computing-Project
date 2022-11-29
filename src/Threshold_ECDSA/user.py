db4a88de1e8daf55f80d660422285542b74bf0f2
CHSimonsen <42768488+CHSimonsen@users.noreply.github.com>
2022-11-29 13:10:56 +0100

    Made a simple wallet.

--- src/Threshold_ECDSA/user.py
+++ src/Threshold_ECDSA/user.py
@@ -1,9 +1,41 @@
+from src.Threshold_ECDSA.threshold_ecdsa import threshold_ecdsa
+import json
 class User:
-    def __init__(self, name):
+    def __init__(self, id, protocol: threshold_ecdsa):
+        self.protocol = protocol
+        # self.id = str(random.randint(1, 100000000)) # TODO: Unique ID - how?
+        self.id = id # TODO: Fake it: They choose their own IDs
         self.wallet = 1000
+        self.pk = None
 
-    def transfer(self):
-        pass
+    def transfer(self, amount, to_id):
+        print(self.wallet)
+        self.wallet -= amount
+        amount_as_string = str(amount)
 
-    def sign(self, message):
-        pass
+        #NOTE: We are not using the IDs right now.
+        to_id_as_string = str(to_id)
+        from_id_as_string = str(self.id)
+        message = '{' + '"amount": ' + amount_as_string + ', "to_id": ' + '"' + to_id_as_string + '"' + ', "from_id": ' + '"' + from_id_as_string + '"' + '}'
+        message = message.encode("utf8")
+        r_x, s = self.protocol.sign(message)
+        print(self.wallet)
+        print("----------")
+        return message, r_x, s, self.pk
+
+    def receive(self, message, r_x, s, pk):
+        print(self.wallet)
+        result = self.protocol.verify(message, r_x, s, pk)
+        message_as_json_object = json.loads(message)
+        amount = message_as_json_object["amount"]
+
+        #NOTE: We are not using the IDs.
+        to_id_as_string = message_as_json_object["to_id"]
+        from_id_as_string = message_as_json_object["from_id"]
+        if result:
+            self.wallet = self.wallet + amount
+        print(self.wallet)
+
+
+    def set_pk(self, pk):
+        self.pk = pk
--- src/main.py
+++ src/main.py
@@ -1,9 +1,20 @@
 from src.Threshold_ECDSA.threshold_ecdsa import threshold_ecdsa
+from src.Threshold_ECDSA.user import User
 
 if __name__ == '__main__':
     threshold_ECDSA = threshold_ecdsa()
-    pk = threshold_ECDSA.key_generation()
-    message = "42".encode("utf8")
-    r_x, s = threshold_ECDSA.sign(message)
-    result = threshold_ECDSA.verify(message, r_x, s, pk)
-    print(result)
+    #TODO: Protocol should hold a dictionary or something on who has what keys.
+
+    user1 = User("Alice", threshold_ECDSA)
+    user1.set_pk(threshold_ECDSA.key_generation())
+
+    user2 = User("Bob", threshold_ECDSA)
+
+    message_to_verify, r_x, s, pk = user1.transfer(100, "Bob")
+    user2.receive(message_to_verify, r_x, s, pk)
+
+
+    # message = "42".encode("utf8")
+    # r_x, s = threshold_ECDSA.sign(message)
+    # result = threshold_ECDSA.verify(message, r_x, s, pk)
+    # print(result)
